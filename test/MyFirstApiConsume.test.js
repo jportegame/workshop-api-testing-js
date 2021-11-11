19de1de0fd80cb98a714a8430dd69cf6cd35bac3
Juan Pablo Ortega Medina <jportegame@unal.edu.co>
2021-11-11 02:58:06 -0500

    My first api consume

---
+++ test/MyFirstApiConsume.test.js
@@ -0,0 +1,59 @@
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('First Api Tests', () => {
+
+    const query = {
+        name: 'John',
+        age: '31',
+        city: 'New York'
+      };
+
+    it('Consume GET Service', async () => {
+        const response = await agent.get('https://httpbin.org/ip');
+        
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body).to.have.property('origin');
+    });
+    
+    it('Consume GET Service with query parameters', async () => {
+      
+        const response = await agent.get('https://httpbin.org/get').query(query);
+      
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.args).to.eql(query);
+    });
+
+    it('Consume HEAD Service', async () => {
+        const response = await agent.head('https://httpbin.org/ip');
+        
+        expect(response.status).to.equal(statusCode.OK);
+    });
+
+    it('Consume PATCH Service', async () => {
+        const response = await agent.patch('https://httpbin.org/patch').send(query);
+
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body).to.have.property('origin');
+        expect(response.body.json).to.eql(query);
+    });
+
+    it('Consume PUT Service', async () => {
+        const response = await agent.put('https://httpbin.org/put').send(query);
+
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body).to.have.property('origin');
+        expect(response.body.json).to.eql(query);
+    });
+
+    it('Consume DELETE Service', async () => {
+        const response = await agent.del('https://httpbin.org/delete').send(query);
+
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body).to.have.property('origin');
+        expect(response.body.json).to.eql(query);
+    });
+});
